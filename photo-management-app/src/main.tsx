652b3124b1457290eaf524dc420db8476c882e1e
Faisal <zkryfysl@gmail.com>
2025-02-11 09:49:25 +0200

    use app.tsx

--- photo-management-app/src/main.tsx
+++ photo-management-app/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./router";
+
 import "./styles/global.css";
 import "./index.css";
+import App from "./App";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AppRouter />
+    <App />
   </React.StrictMode>
 );

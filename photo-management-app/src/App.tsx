c230a331f9f2585361ec83260f31546647493973
Faisal <zkryfysl@gmail.com>
2025-02-11 09:40:26 +0200

    deal with app.tsx , not router.tsx

--- photo-management-app/src/App.tsx
+++ photo-management-app/src/App.tsx
@@ -1,16 +1,20 @@
-// import { Routes, Route } from "react-router-dom";
-// import LoginPage from "./pages/LoginPage";
-// import HomePage from "./pages/HomePage";
-// import RegisterPage from "./pages/RegisterPage";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import UploadPage from "./pages/UploadPage";
 
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<HomePage />} />
-//       <Route path="/login" element={<LoginPage />} />
-//       <Route path="/register" element={<RegisterPage />} />
-//     </Routes>
-//   );
-// }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-// export default App;
+export default App;

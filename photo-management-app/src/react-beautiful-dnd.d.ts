549214dd03ab417d439fb37865d8cad69a8f251c
RP-Line <ArpineAbualrub@gmail.com>
2025-02-15 19:02:07 +0200

    commit- arpi

---
+++ photo-management-app/src/react-beautiful-dnd.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-beautiful-dnd' {
+    // This will give the module an 'any' type, making TypeScript happy, but without strict typing
+    const value: any;
+    export default value;
+  }
